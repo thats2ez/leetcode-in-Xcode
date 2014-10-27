5ff8d975c7e408877b0a27244a316a4f8483962b
Kaiqi <thats2ez@gmail.com>
2014-10-27 11:31:10 -0700

    Initial Commit

---
+++ leetcodeInXcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcodeInXcode
+//
+//  Created by Kaiqi on 14/10/27.
+//  Copyright (c) 2014年 edu.self. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
