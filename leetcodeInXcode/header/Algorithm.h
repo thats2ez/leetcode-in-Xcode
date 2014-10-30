9b05b217d1740a61a9e802f7e483a22afeb36784
Kaiqi <thats2ez@gmail.com>
2014-10-30 00:47:31 -0700

    update git ignore

---
+++ leetcodeInXcode/header/Algorithm.h
@@ -0,0 +1,14 @@
+//
+//  Algorithms.h
+//  leetcodeInXcode
+//
+//  Created by Kaiqi on 14/10/29.
+//  Copyright (c) 2014年 edu.self. All rights reserved.
+//
+
+#ifndef leetcodeInXcode_Algorithms_h
+#define leetcodeInXcode_Algorithms_h
+
+#include <algorithm>
+
+#endif
