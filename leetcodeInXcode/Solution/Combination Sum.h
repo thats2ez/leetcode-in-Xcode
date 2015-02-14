7d40b8cb2bb132b7ddcc5987e0c32d54463c9329
Kaiqi <thats2ez@gmail.com>
2015-02-14 12:46:43 -0800

    Combination Sum

--- leetcodeInXcode/Solution/Combination Sum.h
+++ leetcodeInXcode/Solution/Combination Sum.h
@@ -6,28 +6,64 @@
 //  Copyright (c) 2014 edu.self. All rights reserved.
 //
 
+//class Solution {
+//public:
+//    vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
+//        vector<vector<int> > result;
+//        vector<int> path;
+//        sort(candidates.begin(), candidates.end());
+//        recursiveSum(result, candidates, path, 0, target);
+//        return result;
+//    }
+//    
+//    void recursiveSum(vector<vector<int> > &result, vector<int> &candidates, vector<int> &path, int valid, int remaining) {
+//        // valid result
+//        if (remaining == 0 && !path.empty()) {
+//            result.push_back(path);
+//            return;
+//        }
+//        
+//        // for every valid candidates
+//        for (int i = valid; i < candidates.size() && remaining >= candidates[i]; i++) {
+//            path.push_back(candidates[i]);
+//            recursiveSum(result, candidates, path, i, remaining - candidates[i]);
+//            path.pop_back();
+//        }
+//    }
+//};
+
 class Solution {
 public:
     vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
-        vector<vector<int> > result;
+        sort(begin(candidates), end(candidates));
+        vector<vector<int>>result;
         vector<int> path;
-        sort(candidates.begin(), candidates.end());
-        recursiveSum(result, candidates, path, 0, target);
+        recurisveComb(candidates, target, 0, 0, result, path);
         return result;
     }
     
-    void recursiveSum(vector<vector<int> > &result, vector<int> &candidates, vector<int> &path, int valid, int remaining) {
-        // valid result
-        if (remaining == 0 && !path.empty()) {
+private:
+    void recurisveComb(vector<int> &candidates, int target, int sum, int k, vector<vector<int>> &result, vector<int> &path) {
+        // no answer
+        if (k >= candidates.size() || candidates[k] > target - sum) return;
+        
+        // find one
+        if (candidates[k] == target - sum) {
+            path.push_back(candidates[k]);
             result.push_back(path);
+            path.pop_back();
             return;
         }
         
-        // for every valid candidates
-        for (int i = valid; i < candidates.size() && remaining >= candidates[i]; i++) {
-            path.push_back(candidates[i]);
-            recursiveSum(result, candidates, path, i, remaining - candidates[i]);
-            path.pop_back();
-        }
+        // add current
+        path.push_back(candidates[k]);
+        sum += candidates[k];
+        recurisveComb(candidates, target, sum, k, result, path);
+        path.pop_back();
+        sum -= candidates[k];
+        
+        // not add current
+        recurisveComb(candidates, target, sum, k+1, result, path);
+        return;
     }
 };
