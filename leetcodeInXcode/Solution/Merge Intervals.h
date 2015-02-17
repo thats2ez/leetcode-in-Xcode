cf12032c2d1f9827f6cedfe4997632700acaa79d
Kaiqi <thats2ez@gmail.com>
2015-02-16 19:21:12 -0800

    Merge Intervals

--- leetcodeInXcode/Solution/Merge Intervals.h
+++ leetcodeInXcode/Solution/Merge Intervals.h
@@ -42,3 +42,32 @@ public:
         return result;
     }
 };
+
+///**
+// * Definition for an interval.
+// * struct Interval {
+// *     int start;
+// *     int end;
+// *     Interval() : start(0), end(0) {}
+// *     Interval(int s, int e) : start(s), end(e) {}
+// * };
+// */
+//class Solution {
+//public:
+//    vector<Interval> merge(vector<Interval> &intervals) {
+//        sort(intervals.begin(), intervals.end(), cmp);
+//        vector<Interval> result;
+//        for (int i = 0; i < intervals.size(); i++) {
+//            if (result.empty() || intervals[i].start > result.back().end) {
+//                result.push_back(intervals[i]);
+//            } else {
+//                result.back().end = max(result.back().end, intervals[i].end);
+//            }
+//        }
+//        return result;
+//    }
+//private:
+//    static bool cmp(const Interval &a, const Interval &b) {
+//        return a.start < b.start;
+//    }
+//};
