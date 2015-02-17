d93e3f64c4b98da2254c476cd6ccf4c36b2ba499
Kaiqi <thats2ez@gmail.com>
2015-02-16 18:27:10 -0800

    Maximum Subarray

--- leetcodeInXcode/Solution/Maximum Subarray.h
+++ leetcodeInXcode/Solution/Maximum Subarray.h
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int maxSubArray(int A[], int n) {
-        return 0;
+        if (n <= 0) return 0;
+        
+        int total = 0, sum = INT_MIN, min_total = 0;
+        for (int i = 0; i < n; i++) {
+            total += A[i];
+            sum = max(sum, total - min_total);
+            min_total = min(min_total, total);
+        }
+        return sum;
     }
+    
 };
