02f08d856863b111df769687f9376fef88bf4b76
Kaiqi <thats2ez@gmail.com>
2015-02-17 00:11:12 -0800

    Add Binary

--- leetcodeInXcode/Solution/Add Binary.h
+++ leetcodeInXcode/Solution/Add Binary.h
@@ -12,8 +12,8 @@ public:
         string c;
         c.reserve(max(a.size(), b.size()));
         for (int i = 0, advance = 0; i < a.size() || i < b.size() || advance > 0; i++) {
-            if (i < a.size()) advance += a[i] - '0';
-            if (i < b.size()) advance += b[i] - '0';
+            if (i < a.size()) advance += a[a.size() - 1 - i] - '0';
+            if (i < b.size()) advance += b[b.size() - 1 - i] - '0';
             if (advance % 2 == 0) {
                 c += '0';
             } else {
--- leetcodeInXcode/main.cpp
+++ leetcodeInXcode/main.cpp
@@ -6,13 +6,10 @@
 //  Copyright (c) 2014年 edu.self. All rights reserved.
 //
 
-#include "Rotate List.h"
+#include "Add Binary.h"
 
 int main() {
     Solution s;
-    ListNode head(1);
-    head.next = new ListNode(2);
-    head.next->next = new ListNode(3);
-    auto result = s.rotateRight(&head, 2);
+    auto result = s.addBinary("1010", "1011");
     return 0;
 }
