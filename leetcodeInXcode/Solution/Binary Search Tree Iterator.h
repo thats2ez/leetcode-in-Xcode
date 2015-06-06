e74521e70b7f688e8c44f076b76d7306180c0b60
Kaiqi <thats2ez@gmail.com>
2015-06-05 21:09:56 -0700

    Binary Search Tree Iterator

--- leetcodeInXcode/Solution/Binary Search Tree Iterator.h
+++ leetcodeInXcode/Solution/Binary Search Tree Iterator.h
@@ -15,53 +15,105 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class BSTIterator {
-public:
-    BSTIterator(TreeNode *root) {
-        push_to_next_smaller(root);
-    }
-    
-    /** @return whether we have a next smallest number */
-    bool hasNext() {
-        return !s.empty();
-    }
-    
-    /** @return the next smallest number */
-    int next() {
-        int result = s.top()->val;
-        while(!s.empty()) {
-            TreeNode *node = s.top();
-            // next node is in right sub-tree
-            if (node->right) {
-                push_to_next_smaller(node->right);
-                return result;
-            }
-            // look for next node in stack
-            s.pop();
-            while (!s.empty()) {
-                // if node = parent.left, next node is the parent
-                if (node == s.top()->left)
-                    return result;
-                // if node = parent.right, further pop stack
-                node = s.top();
-                s.pop();
-            }
-        }
-        return result;
-    }
-private:
-    stack<TreeNode *> s;
-    
-    void push_to_next_smaller(TreeNode *node) {
-        while(node != nullptr) {
-            s.push(node);
-            node = node->left;
-        }
-    }
-};
+//class BSTIterator {
+//public:
+//    BSTIterator(TreeNode *root) {
+//        push_to_next_smaller(root);
+//    }
+//    
+//    /** @return whether we have a next smallest number */
+//    bool hasNext() {
+//        return !s.empty();
+//    }
+//    
+//    /** @return the next smallest number */
+//    int next() {
+//        int result = s.top()->val;
+//        while(!s.empty()) {
+//            TreeNode *node = s.top();
+//            // next node is in right sub-tree
+//            if (node->right) {
+//                push_to_next_smaller(node->right);
+//                return result;
+//            }
+//            // look for next node in stack
+//            s.pop();
+//            while (!s.empty()) {
+//                // if node = parent.left, next node is the parent
+//                if (node == s.top()->left)
+//                    return result;
+//                // if node = parent.right, further pop stack
+//                node = s.top();
+//                s.pop();
+//            }
+//        }
+//        return result;
+//    }
+//private:
+//    stack<TreeNode *> s;
+//    
+//    void push_to_next_smaller(TreeNode *node) {
+//        while(node != nullptr) {
+//            s.push(node);
+//            node = node->left;
+//        }
+//    }
+//};
 
 /**
  * Your BSTIterator will be called like this:
  * BSTIterator i = BSTIterator(root);
  * while (i.hasNext()) cout << i.next();
  */
+
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ * Example of iterate a tree:
+ * Solution iterator = Solution(root);
+ * while (iterator.hasNext()) {
+ *    TreeNode * node = iterator.next();
+ *    do something for node
+ */
+class Solution {
+public:
+    //@param root: The root of binary tree.
+    Solution(TreeNode *root) {
+        // write your code here
+        pushLeft(root);
+    }
+    
+    //@return: True if there has next node, or false
+    bool hasNext() {
+        // write your code here
+        return !s.empty();
+    }
+    
+    //@return: return next node
+    TreeNode* next() {
+        // write your code here
+        TreeNode *node = s.top();
+        s.pop();
+        pushLeft(node->right);
+        return node;
+    }
+    
+private:
+    stack<TreeNode *> s;
+    
+    void pushLeft(TreeNode *node) {
+        while (node) {
+            TreeNode *next = node->left;
+            s.push(node);
+            node = next;
+        }
+    }
+};
