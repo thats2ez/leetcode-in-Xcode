b446451d01d8b2af1e5a54e81d6ccb6d0a06531a
Kaiqi <thats2ez@gmail.com>
2015-07-11 15:51:40 -0700

    Number of Digit One

--- leetcodeInXcode/Solution/Number of Digit One.h
+++ leetcodeInXcode/Solution/Number of Digit One.h
@@ -6,8 +6,19 @@
 //  Copyright (c) 2015 edu.self. All rights reserved.
 //
 
-#ifndef leetcodeInXcode_Number_of_Digit_One_h
-#define leetcodeInXcode_Number_of_Digit_One_h
-
-
-#endif
+class Solution {
+public:
+    int countDigitOne(int n) {
+//        cout << n << endl;
+        if (n <= 0) return 0;
+        long long a = 0;
+        long long base = 1;
+        while (base * 10 <= n) {
+            a = base + a * 10;
+            base *= 10;
+        }
+        int remain = n % base;
+        int result = (int)a * (n / base) + countDigitOne(remain) + (int)min(base, n - base + 1);
+        return result;
+    }
+};
